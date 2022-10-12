61ea01b68ced1de6d3f464a1679c5873be320a09
Huynh Phat <phathuynhz456@gmail.com>
2022-10-12 13:39:43 +0700

    finished lấy dữ liệu người dùng nhập vào của Layout AddFilm

--- movie_web/src/Page/MoviePage/AddFilm.js
+++ movie_web/src/Page/MoviePage/AddFilm.js
@@ -11,9 +11,66 @@ import {
   TreeSelect,
 } from "antd";
 import React, { useState } from "react";
+import { useFormik } from "formik";
+import moment from "moment";
 
 export default function AddFilm() {
   const [componentSize, setComponentSize] = useState("default");
+  const [imgSrc, setimgSrc] = useState("");
+
+  const formik = useFormik({
+    initialValues: {
+      tenPhim: "",
+      trailer: "",
+      moTa: "",
+      ngayKhoiChieu: "",
+      dangChieu: false,
+      sapChieu: false,
+      hot: false,
+      danhGia: 0,
+      hinhAnh: {},
+    },
+    onSubmit: (values) => {
+      console.log("values: ", values);
+    },
+  });
+
+  const handleChangeDataPicker = (values) => {
+    let ngayKhoiChieu = moment(values).format("DD/MM/YYYY");
+    console.log("ngayKhoiChieu: ", ngayKhoiChieu);
+    formik.setFieldValue("ngayKhoiChieu", ngayKhoiChieu);
+  };
+
+  const handleChangeSwitch = (name) => {
+    return (value) => {
+      formik.setFieldValue(name, value);
+    };
+  };
+  const handleChangeInputNumber = (name) => {
+    return (value) => {
+      formik.setFieldValue(name, value);
+    };
+  };
+
+  const handleChangeFile = (e) => {
+    let fileimg = e.target.files[0];
+    if (
+      fileimg.type === "image/png" ||
+      fileimg.type === "image/jpeg" ||
+      fileimg.type === "image/jpg" ||
+      fileimg.type === "image/gif"
+    ) {
+      let reader = new FileReader();
+      reader.readAsDataURL(fileimg);
+      reader.onload = (e) => {
+        setimgSrc(e.target.result);
+      };
+      formik.setFieldValue("hinhAnh", fileimg);
+    } else {
+      alert("Dữ liệu không phù hợp");
+    }
+    console.log("fileimg: ", fileimg);
+  };
 
   const onFormLayoutChange = ({ size }) => {
     setComponentSize(size);
@@ -21,6 +78,7 @@ export default function AddFilm() {
 
   return (
     <Form
+      onSubmitCapture={formik.handleSubmit}
       labelCol={{
         span: 4,
       }}
@@ -44,43 +102,55 @@ export default function AddFilm() {
       </Form.Item>
 
       <Form.Item label="Tên phim">
-        <Input name="tenPhim" />
+        <Input name="tenPhim" onChange={formik.handleChange} />
       </Form.Item>
 
       <Form.Item label="Trailer phim">
-        <Input name="trailer" />
+        <Input name="trailer" onChange={formik.handleChange} />
       </Form.Item>
 
       <Form.Item label="Mô tả phim">
-        <Input name="moTa" />
+        <Input name="moTa" onChange={formik.handleChange} />
       </Form.Item>
 
       <Form.Item label="Ngày khởi chiếu">
-        <DatePicker />
+        <DatePicker format={"DD/MM/YYYY"} onChange={handleChangeDataPicker} />
       </Form.Item>
 
       <Form.Item label="Đang chiếu" valuePropName="checked">
-        <Switch />
+        <Switch onChange={handleChangeSwitch("dangChieu")} />
       </Form.Item>
 
       <Form.Item label="Sắp chiếu" valuePropName="checked">
-        <Switch />
+        <Switch onChange={handleChangeSwitch("sapChieu")} />
       </Form.Item>
 
       <Form.Item label="Hot" valuePropName="checked">
-        <Switch />
+        <Switch onChange={handleChangeSwitch("hot")} />
       </Form.Item>
 
       <Form.Item label="Đánh giá sao">
-        <InputNumber />
+        <InputNumber
+          onChange={handleChangeInputNumber("danhGia")}
+          min={0}
+          max={10}
+        />
       </Form.Item>
 
       <Form.Item label="Hình ảnh">
-        <input type="file" />
+        <input
+          type="file"
+          onChange={handleChangeFile}
+          accept="image/png, image/jpeg, image/gif"
+        />
+        <br />
+        <img style={{ width: 200, height: 200 }} src={imgSrc} alt="..." />
       </Form.Item>
 
-      <Form.Item label="Button">
-        <Button>Button</Button>
+      <Form.Item label="Tác vụ">
+        <Button type="primary" htmlType="submit">
+          Thêm Phim
+        </Button>
       </Form.Item>
     </Form>
   );
