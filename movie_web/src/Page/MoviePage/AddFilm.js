7610fe27e8dbd4f70809548556e672ad28e05cbd
Huynh Phat <phathuynhz456@gmail.com>
2022-10-12 14:55:02 +0700

    Finished validate form AddFiml

--- movie_web/src/Page/MoviePage/AddFilm.js
+++ movie_web/src/Page/MoviePage/AddFilm.js
@@ -1,18 +1,16 @@
 import {
   Button,
-  Cascader,
   DatePicker,
   Form,
   Input,
   InputNumber,
   Radio,
-  Select,
   Switch,
-  TreeSelect,
 } from "antd";
 import React, { useState } from "react";
 import { useFormik } from "formik";
 import moment from "moment";
+import * as Yup from "yup";
 
 export default function AddFilm() {
   const [componentSize, setComponentSize] = useState("default");
@@ -30,6 +28,22 @@ export default function AddFilm() {
       danhGia: 0,
       hinhAnh: {},
     },
+    validationSchema: Yup.object({
+      tenPhim: Yup.string()
+        .min(5, "tên không được ít hơn 5 kí tự")
+        .max(25, "Tên không được dài hơn 25 kí tự")
+        .required("không được để trống tên Phim"),
+      trailer: Yup.string().required("không được để trống trailer phim"),
+      moTa: Yup.string().required("Không được để trống Mô tả phim"),
+      ngayKhoiChieu: Yup.string()
+        .required("Không được để trống Ngày Khởi Chiếu")
+        .matches(
+          /^(?:(?:31(\/|-|\.)(?:0?[13578]|1[02]))\1|(?:(?:29|30)(\/|-|\.)(?:0?[13-9]|1[0-2])\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29(\/|-|\.)0?2\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])(\/|-|\.)(?:(?:0?[1-9])|(?:1[0-2]))\4(?:(?:1[6-9]|[2-9]\d)?\d{2})$/,
+          "Ngày không hợp lệ!!!"
+        ),
+      danhGia: Yup.string().required("Không được để trống đánh giá"),
+      hinhAnh: Yup.string().required("Không được để trống hình ảnh"),
+    }),
     onSubmit: (values) => {
       console.log("values: ", values);
     },
@@ -101,20 +115,35 @@ export default function AddFilm() {
         </Radio.Group>
       </Form.Item>
 
-      <Form.Item label="Tên phim">
+      <Form.Item
+        label="Tên phim"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
         <Input name="tenPhim" onChange={formik.handleChange} />
+        {formik.errors.tenPhim && (
+          <p className="text-red-500">{formik.errors.tenPhim}</p>
+        )}
       </Form.Item>
 
       <Form.Item label="Trailer phim">
         <Input name="trailer" onChange={formik.handleChange} />
+        {formik.errors.trailer && (
+          <p className="text-red-500">{formik.errors.trailer}</p>
+        )}
       </Form.Item>
 
       <Form.Item label="Mô tả phim">
         <Input name="moTa" onChange={formik.handleChange} />
+        {formik.errors.moTa && (
+          <p className="text-red-500">{formik.errors.moTa}</p>
+        )}
       </Form.Item>
 
       <Form.Item label="Ngày khởi chiếu">
         <DatePicker format={"DD/MM/YYYY"} onChange={handleChangeDataPicker} />
+        {formik.errors.ngayKhoiChieu && (
+          <p className="text-red-500">{formik.errors.ngayKhoiChieu}</p>
+        )}
       </Form.Item>
 
       <Form.Item label="Đang chiếu" valuePropName="checked">
@@ -134,7 +163,11 @@ export default function AddFilm() {
           onChange={handleChangeInputNumber("danhGia")}
           min={0}
           max={10}
+          value={formik.values.danhGia}
         />
+        {formik.errors.danhGia && (
+          <p className="text-red-500">{formik.errors.danhGia}</p>
+        )}
       </Form.Item>
 
       <Form.Item label="Hình ảnh">
@@ -143,6 +176,9 @@ export default function AddFilm() {
           onChange={handleChangeFile}
           accept="image/png, image/jpeg, image/gif"
         />
+        {formik.errors.hinhAnh && (
+          <p className="text-red-500">Không được để trống hình ảnh</p>
+        )}
         <br />
         <img style={{ width: 200, height: 200 }} src={imgSrc} alt="..." />
       </Form.Item>
