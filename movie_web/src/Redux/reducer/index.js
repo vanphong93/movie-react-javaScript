497c4704efa9429f4a3d780664324c270346d0e8
Ho Van Phong <phongckm93@gmail.com>
2022-10-14 09:12:08 +0700

    Revert "Merge branch 'clone_phong93' into 'main'"
    This reverts commit 36adafb61485b8c5f31bd85df6d220ae2c41f86f, reversing
    changes made to eaac845f34b9ccfd41e1a460b088f29fe2998cf0.

---
+++ movie_web/src/Redux/reducer/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+import { userReducer } from "./reducerUser";
+export let rootReducer = combineReducers({
+  userReducer
+});
--- movie_web_Phong/src/redux/constant/constantSpiner.js
+++
@@ -1,2 +0,0 @@
-export const SET_LOADING_ON="set_loading_on"
-export const SET_LOADING_OFF="set_loading_off"
