dcf54e37d83946fabc259a6415219171cea3bece
Ho Van Phong <phongckm93@gmail.com>
2022-10-29 20:18:31 +0700

    save theme to localStore

--- movie_web/src/HOC/Layout.js
+++ movie_web/src/HOC/Layout.js
@@ -1,13 +1,24 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Components/Footer/Footer";
 import Header from "../Components/HeaderThemes/Header";
 import ScrollButton from "../Components/ScrollTop/ScrollBtn";
 
 export default function Layout({ children }) {
   const [theme, setTheme] = useState(false);
+  useEffect(() => {
+    let currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+      setTheme(true);
+    }
+  }, []);
   let changeTheme = () => {
+    if (!theme) {
+      localStorage.setItem("theme", "dark");
+    } else {
+      localStorage.setItem("theme", "light");
+    }
     setTheme(!theme);
-    return setTheme(!theme)
+    return setTheme(!theme);
   };
   return (
     <div className={theme ? "dark" : ""}>
